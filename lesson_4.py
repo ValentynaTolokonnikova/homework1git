6c927e5aa816d7b62d212fa6e5ce9b05e380b8d3
ValentynaTolokonnikova <valeholyhands@gmail.com>
2024-01-29 13:43:53 +0200

    added checking for empty list in task 2

--- lesson_4.py
+++ lesson_4.py
@@ -1,6 +1,6 @@
 # # Task 4.1
 
-value_list_1 = [0, 5, 0, 6, 7]
+value_list_1 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 for num in value_list_1:
     if num == 0:
         value_list_1.remove(0)
@@ -10,7 +10,7 @@ print(value_list_1)
 
 # # Task 4.2
 
-list_1 = [0]                               #исправлен, добавлена проверка на пустой список
+list_1 = []                               #исправлен, добавлена проверка на пустой список
 if not list_1:
     list_1 = 0
     print(list_1)
